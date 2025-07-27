464e04d094561e07fd58b56c2556300d25555dbc
Smartbus-360 <smartbus365@gmail.com>
2025-07-27 19:53:11 +0530

    Update admin.controller.js

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -25,7 +25,7 @@ export const getAdmins = async (req, res, next) => {
         u.nationality,
         i.name AS instituteName,
         i.institutionType,
-        i.instituteCode
+        i.instituteCode,
       FROM 
         tbl_sm360_users u
       LEFT JOIN 
