e91715a9c8086d1d92ab5df9c187b2bf1fd19038
Smartbus-360 <smartbus365@gmail.com>
2025-08-22 10:59:47 +0530

    Update api.route.js

--- routes/api.route.js
+++ routes/api.route.js
@@ -14,8 +14,12 @@ apiRouter.post("/login/user",  loginUser);
 apiRouter.post("/login/driver", loginDriver);
 apiRouter.post("/signup/driver", signupUser);
 apiRouter.post("/signup/user", signupDriver);
-apiRouter.get("/user/details/:id", httpAuth,canViewMap, getUserDetails);
-apiRouter.get("/driver/details/:id", httpAuth,canViewMap, getDriverDetails);
+apiRouter.get("/user/details/:id", httpAuth, getUserDetails);
+apiRouter.get("/driver/details/:id", httpAuth, getDriverDetails);
+// ⬇️ put this near your other routes
+apiRouter.get("/map/access-check", httpAuth, canViewMap, (req, res) => {
+  return res.status(204).end(); // 204 when map is allowed
+});
 apiRouter.post("/stoppage/reached", httpAuth,canViewMap, updateReachDateTime); 
 apiRouter.post("/notify/speed", httpAuth,canViewMap, notifyIfSpeedExceeded);
 apiRouter.get("/advertisement/banner", generateAdBanner);
@@ -23,8 +27,8 @@ apiRouter.get("/bus/replacement/:busId", httpAuth,canViewMap, checkBusReplacemen
 apiRouter.post("/missed-stoppage", httpAuth,canViewMap, markMissedStop);
 apiRouter.post("/mark-final-stop",httpAuth,canViewMap, markFinalStopReached);
 apiRouter.get("/reach-times/:route", httpAuth,canViewMap ,getReachTimesForRoute);
-apiRouter.get("/notifications", httpAuth,canViewMap ,getNotifications);
-apiRouter.get("/bus-notifications", httpAuth,canViewMap, getBusNotifications);
+apiRouter.get("/notifications", httpAuth,getNotifications);
+apiRouter.get("/bus-notifications", httpAuth, getBusNotifications);
 apiRouter.post('/one-time-login', oneTimeLogin);
 
 export default apiRouter;
