98b020c309b99b7d9d097a6d416ea1086c2e1979
Smartbus-360 <smartbus365@gmail.com>
2025-10-01 15:07:02 +0530

    Update api.route.js

--- routes/api.route.js
+++ routes/api.route.js
@@ -13,6 +13,7 @@ import {
   revokeDriverQr          // POST /driver-qr/revoke/:id
 } from "../controllers/driverQr.controller.js";
 import { getDriverSelf } from "../controllers/driver.controller.js";
+import { markStopReached } from "../controllers/stoppage.controller.js";
 
 
 const apiRouter = express.Router();
@@ -47,6 +48,7 @@ apiRouter.post("/driver-qr/revoke/:id",  httpAuth, revokeDriverQr);
 apiRouter.post('/one-time-login', oneTimeLogin);
 // apiRouter.post("/auth/refresh", refreshAccessToken);
 // apiRouter.post("/auth/logout", httpAuth, logout);
+apiRouter.post("/stoppage/reached", httpAuth, markStopReached);
 
 
 
