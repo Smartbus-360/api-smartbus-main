68404454f724738aedf3b81f069b2b25c82ad78a
Smartbus-360 <smartbus365@gmail.com>
2025-10-26 15:53:57 +0530

    Create attendanceTaker.model.js

---
+++ models/attendanceTaker.model.js
@@ -0,0 +1,27 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+import Institute from './institute.model.js';
+
+const AttendanceTaker = sequelize.define('tbl_sm360_attendance_takers', {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
+  phone: { type: DataTypes.STRING, allowNull: false },
+  instituteId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: { model: Institute, key: 'id' },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  availabilityStatus: {
+    type: DataTypes.ENUM('Available', 'Unavailable'),
+    allowNull: false,
+    defaultValue: 'Available',
+  },
+  token: { type: DataTypes.STRING, allowNull: true },
+  lastLogin: { type: DataTypes.DATE, allowNull: true },
+}, { timestamps: true });
+
+export default AttendanceTaker;
