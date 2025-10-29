81006b39793e5225856b54cbe8bf02c63f99c531
Smartbus-360 <smartbus365@gmail.com>
2025-10-30 02:05:50 +0530

    Update attendanceTakerAttendanceTemp.model.js

--- models/attendanceTakerAttendanceTemp.model.js
+++ models/attendanceTakerAttendanceTemp.model.js
@@ -1,33 +1,54 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
 
-// ✅ No import of AttendanceTaker here!
-
 const AttendanceTakerAttendanceTemp = sequelize.define(
   "AttendanceTakerAttendanceTemp",
   {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    registrationNumber: { type: DataTypes.STRING, allowNull: false },
-    username: { type: DataTypes.STRING },
-    instituteName: { type: DataTypes.STRING },
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    registrationNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    instituteName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
     attendance_taker_id: {
       type: DataTypes.INTEGER,
+      allowNull: true,
       references: { model: "tbl_sm360_attendance_takers", key: "id" },
       onUpdate: "CASCADE",
       onDelete: "SET NULL",
     },
-    bus_id: { type: DataTypes.STRING },
-    latitude: { type: DataTypes.FLOAT },
-    longitude: { type: DataTypes.FLOAT },
-    scan_time: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    bus_id: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    latitude: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    longitude: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    scan_time: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
   },
   {
-    tableName: "tbl_sm360_driver_attendance_temp",
     timestamps: true,
+    tableName: "tbl_sm360_driver_attendance_temp",
   }
 );
-
-// ✅ DO NOT call belongsTo here!
-// Association will be created in models/index.js instead.
 
 export default AttendanceTakerAttendanceTemp;
