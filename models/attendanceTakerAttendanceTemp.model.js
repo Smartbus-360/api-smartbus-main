636eb76523f0b2b1a3cbb96d4dc0cb1a05a89624
Smartbus-360 <smartbus365@gmail.com>
2025-10-30 01:59:33 +0530

    Update attendanceTakerAttendanceTemp.model.js

--- models/attendanceTakerAttendanceTemp.model.js
+++ models/attendanceTakerAttendanceTemp.model.js
@@ -1,58 +1,10 @@
-// import { DataTypes } from 'sequelize';
-// import sequelize from '../config/database.js';
-// import AttendanceTaker from "./attendanceTaker.model.js";
-
-// const AttendanceTakerAttendanceTemp = sequelize.define('tbl_sm360_driver_attendance_temp', {
-//   id: {
-//     type: DataTypes.INTEGER,
-//     autoIncrement: true,
-//     primaryKey: true,
-//   },
-//   registrationNumber: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   username: {
-//     type: DataTypes.STRING,
-//     allowNull: true,
-//   },
-//   instituteName: {
-//     type: DataTypes.STRING,
-//     allowNull: true,
-//   },
-//   attendance_taker_id: {
-//     type: DataTypes.INTEGER,
-//     allowNull: true,
-//     references: { model: "tbl_sm360_attendance_takers", key: "id" },
-//     onUpdate: "CASCADE",
-//     onDelete: "SET NULL",
-//   },
-//   bus_id: {
-//     type: DataTypes.STRING,
-//     allowNull: true,
-//   },
-//   latitude: {
-//     type: DataTypes.FLOAT,
-//     allowNull: true,
-//   },
-//   longitude: {
-//     type: DataTypes.FLOAT,
-//     allowNull: true,
-//   },
-//   scan_time: {
-//     type: DataTypes.DATE,
-//     defaultValue: DataTypes.NOW,
-//   },
-// }, {
-//   timestamps: true,
-//   tableName: 'tbl_sm360_driver_attendance_temp',
-// });
-
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
+import AttendanceTaker from "./attendanceTaker.model.js";
 
+// Define Model
 const AttendanceTakerAttendanceTemp = sequelize.define(
-  "tbl_sm360_driver_attendance_temp",
+  "AttendanceTakerAttendanceTemp",
   {
     id: {
       type: DataTypes.INTEGER,
@@ -71,9 +23,12 @@ const AttendanceTakerAttendanceTemp = sequelize.define(
       type: DataTypes.STRING,
       allowNull: true,
     },
-    attendance_taker: {
-      type: DataTypes.STRING, // ✅ plain text (no foreign key)
+    attendance_taker_id: {
+      type: DataTypes.INTEGER,
       allowNull: true,
+      references: { model: "tbl_sm360_attendance_takers", key: "id" },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
     },
     bus_id: {
       type: DataTypes.STRING,
@@ -98,10 +53,10 @@ const AttendanceTakerAttendanceTemp = sequelize.define(
   }
 );
 
+// ✅ Define association AFTER the model
+AttendanceTakerAttendanceTemp.belongsTo(AttendanceTaker, {
+  foreignKey: "attendance_taker_id",
+  as: "attendanceTaker",
+});
+
 export default AttendanceTakerAttendanceTemp;
-
-
-export default AttendanceTakerAttendanceTemp;
-
-
-// export default AttendanceTakerAttendanceTemp;
