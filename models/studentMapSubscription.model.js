562b8b9537b1cb0f614b610438c85c54d98e57f8
Smartbus-360 <smartbus365@gmail.com>
2025-12-22 17:59:29 +0530

    Create studentMapSubscription.model.js

---
+++ models/studentMapSubscription.model.js
@@ -0,0 +1,44 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+import User from "./user.model.js";
+
+const StudentMapSubscription = sequelize.define("student_map_subscriptions", {
+  student_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    // references: {
+    //   model: User,
+    //   key: "id",
+    // },
+  },
+  plan_type: {
+    type: DataTypes.ENUM("monthly", "yearly"),
+    allowNull: false,
+  },
+  months: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  amount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  txn_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  start_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  end_date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.ENUM("active", "expired","revoked"),
+    defaultValue: "active",
+  }
+}, { timestamps: true });
+
+export default StudentMapSubscription;
