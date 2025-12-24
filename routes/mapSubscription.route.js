378726ee8a9349bc604d9a21de0c0ed528b904c2
Smartbus-360 <smartbus365@gmail.com>
2025-12-24 14:11:32 +0530

    Update mapSubscription.route.js

--- routes/mapSubscription.route.js
+++ routes/mapSubscription.route.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { httpAuth } from "../middleware/wsAuth.middleware.js";
 import {
-  activateStudentMapSubscription,
   getStudentSubscriptionHistory,
   getMapSubscriptionPlans,
   checkMapAccess
@@ -11,7 +10,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 // Student
-router.post("/map/subscription/activate", httpAuth, activateStudentMapSubscription);
+// router.post("/map/subscription/activate", httpAuth, activateStudentMapSubscription);
 router.get("/map/subscription/history", httpAuth, getStudentSubscriptionHistory);
 router.get("/map/subscription/plans", httpAuth, getMapSubscriptionPlans);
 router.get("/map/access-check", httpAuth, checkMapAccess);
