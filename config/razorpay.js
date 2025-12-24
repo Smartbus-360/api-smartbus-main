f712b7549afcbffb624555946575d6728a433744
Smartbus-360 <smartbus365@gmail.com>
2025-12-24 15:26:53 +0530

    Update razorpay.js

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,8 +1,27 @@
+// import Razorpay from "razorpay";
+
+// const razorpay = new Razorpay({
+//   key_id: process.env.RAZORPAY_KEY_ID,
+//   key_secret: process.env.RAZORPAY_KEY_SECRET
+// });
+
+// export default razorpay;
+
 import Razorpay from "razorpay";
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET
-});
+let razorpay;
 
-export default razorpay;
+function getRazorpayInstance() {
+  if (!razorpay) {
+    if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
+      throw new Error("Razorpay keys not loaded in environment");
+    }
+
+    razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET
+    });
+  }
+
+  return razorpay;
+}
