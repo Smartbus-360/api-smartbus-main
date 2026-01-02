e36f01439f2c5f7e9599d3e0549530a7033ccaaf
Smartbus-360 <smartbus365@gmail.com>
2026-01-02 11:16:09 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ const sequelize = new Sequelize(
     logging: false,
     timezone: "+05:30", // IST
     dialectOptions: {
-      // useUTC: false,
+      useUTC: false,
       dateStrings: true,   // ✅ Force MySQL to return DATETIME as string (not UTC)
       typeCast: true,      // ✅ So Sequelize doesn't convert timestamps to JS UTC Dates
     },
