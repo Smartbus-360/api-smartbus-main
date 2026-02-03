e9ef89c680d3b2ef42edb6295eb64af3f4f807c7
Smartbus-360 <smartbus365@gmail.com>
2026-02-03 16:56:50 +0530

    Create homepageContent.route.js

---
+++ routes/homepageContent.route.js
@@ -0,0 +1,32 @@
+import express from "express";
+import multer from "multer";
+import { verifyToken } from "../utils/verifyUser.js";
+import {
+  addHomepageContent,
+  getHomepageContent,
+} from "../controllers/homepageContent.controller.js";
+
+const router = express.Router();
+
+// file upload config
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// SUPER ADMIN
+router.post(
+  "/homepage-content",
+  verifyToken,
+  upload.single("image"),
+  addHomepageContent
+);
+
+// PUBLIC
+router.get("/homepage-content", getHomepageContent);
+
+export default router;
