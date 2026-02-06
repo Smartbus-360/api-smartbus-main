716eb0841265876def481d4ffcfa64607a2a854d
Smartbus-360 <smartbus365@gmail.com>
2026-02-06 15:08:15 +0530

    Update and rename homeContent.route.js to homepageContent.route.js

--- routes/homeContent.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createHomeContent } from "../controllers/homeContent.controller.js";
-import { verifyToken } from "../middleware/verifyToken.js";
-
-const router = express.Router();
-
-router.post("/home-content", verifyToken, createHomeContent);
-
-export default router;
